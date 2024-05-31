efe348d6027abdee373ee527a1a941365c61fe26
Purav <puravparabdev@gmail.com>
2024-05-31 05:24:58 -0700

    rendered earth in 3js

---
+++ client/app/components/earth.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { useEffect, useRef} from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const Earth: React.FC = () => {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+
+	useEffect(() => {
+		if (canvasRef.current && containerRef.current){
+			const container = containerRef.current;
+			const width = container.clientWidth;
+      const height = container.clientHeight;
+
+			const scene = new THREE.Scene();
+
+			// set camera
+			const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000);
+			camera.position.z = 4;
+
+			// create renderer
+			const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current, antialias: true });
+			renderer.setSize(width, height);
+			renderer.setClearColor('rgb(2, 2, 2)');
+			
+			const earthGroup = new THREE.Group();
+			scene.add(earthGroup);
+			new OrbitControls(camera, renderer.domElement);
+			
+			// add mesh and materials
+			// lights
+			const geometry = new THREE.IcosahedronGeometry(1, 12);
+      const lights_texture = new THREE.TextureLoader().load('./assets/textures/earth_lights.jpg');
+      const lights_material = new THREE.MeshBasicMaterial({ map: lights_texture });
+			const lights = new THREE.Mesh(geometry, lights_material);
+			earthGroup.add(lights);
+			// grayscale
+      const grayscale_texture = new THREE.TextureLoader().load('./assets/textures/earth_grayscale.jpg');
+      const grayscale_material = new THREE.MeshStandardMaterial({ map: grayscale_texture, blending: THREE.AdditiveBlending});
+			const grayscale = new THREE.Mesh(geometry, grayscale_material);
+			earthGroup.add(grayscale);
+			
+			// lights
+			const ambientLight = new THREE.AmbientLight(0x4e5cd7, 1);
+			earthGroup.add(ambientLight);
+	
+			// create animation
+			const animate = () => {
+				requestAnimationFrame(animate);
+				lights.rotation.y += 0.001;
+				grayscale.rotation.y += 0.001;
+				renderer.render(scene, camera);
+			};
+			animate();
+
+			return () => {renderer.dispose();}; // Clean up on component unmount
+		}
+	}, []);
+
+	return (
+		// <div ref={containerRef} className="w-full h-full border-2 border-slate-700 rounded-md">
+		<div ref={containerRef} className="w-full h-full">
+      <canvas ref={canvasRef} className="w-full h-full rounded-md cursor-pointer"/>
+    </div>
+	);
+}
+
+export default Earth;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Earth from "./components/earth";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <Earth />
     </main>
   );
 }
