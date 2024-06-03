e5044cc0dc4b8e6ea8edb1ad0ddd886a12bc2268
Purav <puravparabdev@gmail.com>
2024-06-03 07:11:46 -0700

    satellites resizing

--- client/app/components/satellites.tsx
+++ client/app/components/satellites.tsx
@@ -13,11 +13,15 @@ const calculateSatPosition = (satrec: satellite.SatRec, date: Date) => {
 		const theta = (satellite.degreesLong(positionGd.longitude)) * (Math.PI / 180);
 		const radius = 1 + positionGd.height / EARTH_RADIUS;
 
-		return new THREE.Vector3 (
-			radius * Math.sin(phi) * Math.cos(theta),
-			radius * Math.cos(phi),
-			-radius * Math.sin(phi) * Math.sin(theta) // negating seems to display orbits correctly
-		);
+		return {
+			position: 
+				new THREE.Vector3 (
+				radius * Math.sin(phi) * Math.cos(theta), // x
+				radius * Math.cos(phi), // y
+				-radius * Math.sin(phi) * Math.sin(theta) // z (negating seems to display orbits correctly)
+			),
+			radius
+		};
 	}
 	return null;
 }
@@ -50,11 +54,16 @@ const renderSatellites = async (scene: THREE.Scene, camera: THREE.Camera, graysc
 		const instanceCount = Object.keys(data).length;
 		console.log("total satellites", instanceCount)
 
-		const geometry = new THREE.SphereGeometry(0.006, 16, 16);
-		const material = new THREE.MeshBasicMaterial({ color: 0x50C878 })
+		const geometry = new THREE.TetrahedronGeometry(0.01);
+		const material = new THREE.MeshBasicMaterial({ color: 0xbbf2a4 })
 		const instancedMesh = new THREE.InstancedMesh(geometry, material, instanceCount);
 		const dummy = new THREE.Object3D();
 
+		// Create an InstancedBufferAttribute for the radii
+		const radii = new THREE.InstancedBufferAttribute(new Float32Array(instanceCount), 1);
+		instancedMesh.instanceMatrix.needsUpdate = true;
+		instancedMesh.geometry.setAttribute('radius', radii);
+		
 		const satelliteData: Satellite[] = []
 		let count = 0;
 		Object.keys(data).forEach((norad_id, index) => {
@@ -77,14 +86,15 @@ const renderSatellites = async (scene: THREE.Scene, camera: THREE.Camera, graysc
 				// get positions of the satellite at every interval i to render an orbit
 				for (let i = 0; i <= orbitalPeriod; i++) {
 					const future = new Date(date.getTime() + (i * 1000)); // datetime requires time in milliseconds
-					const pos = calculateSatPosition(satrec, future);
-					if (pos) {
-						orbitPositions.push(pos)
-					};
+					const satData = calculateSatPosition(satrec, future);
+					if (satData) {
+						const { position } = satData;
+						orbitPositions.push(position);
+      		}
 				}
 				if (orbitPositions.length > 0) {
 					const orbitGeometry = new THREE.BufferGeometry().setFromPoints(orbitPositions);
-					const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 3 });
+					const orbitMaterial = new THREE.LineBasicMaterial({ color: 0xf9cb9c, linewidth: 4 });
 					orbitLine = new THREE.Line(orbitGeometry, orbitMaterial);
 					scene.add(orbitLine);
 				}
@@ -110,11 +120,12 @@ const renderSatellites = async (scene: THREE.Scene, camera: THREE.Camera, graysc
         const instanceId = intersects[0].instanceId;
         if (instanceId !== undefined) {
           const sat = satelliteData[instanceId];
-					const satPosition = calculateSatPosition(sat.satrec, new Date);
-					if (satPosition) {
+					const satData = calculateSatPosition(sat.satrec, new Date);
+					if (satData) {
+						const { position } = satData;
 						// Check if the satellite is occluded by the Earth
 						const earthRaycaster = new THREE.Raycaster();
-						earthRaycaster.set(satPosition, camera.position.clone().sub(satPosition).normalize());
+						earthRaycaster.set(position, camera.position.clone().sub(position).normalize());
 						const earthIntersects = earthRaycaster.intersectObject(grayscale);
 						// The satellite is not occluded by the Earth, so render its orbit
 						if (earthIntersects.length === 0) {
@@ -134,14 +145,18 @@ const renderSatellites = async (scene: THREE.Scene, camera: THREE.Camera, graysc
 			requestAnimationFrame(animate);
 			satelliteData.forEach((sat, index) => {
 				const { satrec } = sat;
-				const pos = calculateSatPosition(satrec, new Date);
-				if (pos) {
-					dummy.position.copy(pos);
+				const satData = calculateSatPosition(satrec, new Date);
+				if (satData) {
+					const { position, radius } = satData;
+					dummy.position.copy(position);
+					dummy.scale.setScalar(0.5 * radius); // Scale the dummy object based on the radius
 					dummy.updateMatrix();
 					instancedMesh.setMatrixAt(index, dummy.matrix);
+					radii.setX(index, radius); // Update the radius value for the instance
 				}
 			});
 			instancedMesh.instanceMatrix.needsUpdate = true;
+			radii.needsUpdate = true; // Mark the radii attribute as needing update
 		};
 		animate();
 
